b2f35e44aa77ab8962e833d524acfbc7f6201521
Joao Carlos <joao_uzae@hotmail.com>
2025-04-12 19:11:07 -0300

    first cut of FT msgs_cleared_on_input

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 5
+MAX_WAIT = 10
 
 
 class FunctionalTest(StaticLiveServerTestCase):
