9b0aa29d6bb13df5cc475a7876a265324f6c9827
Joao Carlos <joao_uzae@hotmail.com>
2025-04-12 17:43:11 -0300

    implement custom save method for the form

--- src/lists/views.py
+++ src/lists/views.py
@@ -12,7 +12,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         nulist = List.objects.create()
-        item = Item.objects.create(text=request.POST["text"], list=nulist)
+        form.save(for_list=nulist)
         return redirect(nulist)
     else:
         return render(request, "home.html", {"form": form})
@@ -23,7 +23,7 @@ def view_list(request, list_id):
     if request.method == "POST":
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST["text"], list=our_list)
+            form.save(for_list=our_list)
             return redirect(our_list)
     else:
         form = ItemForm()
