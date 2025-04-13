2e294fed1d7d61ef2b93a8d198d8245e81de4010
Joao Carlos <joao_uzae@hotmail.com>
2025-04-12 22:52:46 -0300

    Despike our js, add jasmine tests

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+MAX_WAIT = 20
 
 
 class FunctionalTest(StaticLiveServerTestCase):
