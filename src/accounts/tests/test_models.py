d62f3c6137bf0d63069710c66b0181b8c6afc928
Joao Carlos <joao_uzae@hotmail.com>
2025-04-26 14:25:10 -0300

    elimina teste do livro, porque o meu modelo tem senha
    portanto, não é válido só com email

--- src/accounts/tests/test_models.py
+++ src/accounts/tests/test_models.py
@@ -5,10 +5,6 @@ User = get_user_model()
 
 
 class UserModelTest(TestCase):
-    def test_user_is_valid_with_email_only(self):
-        user = User(email="a@b.com")
-        user.full_clean()  # should not raise
-
     def test_email_is_primary_key(self):
         user = User(email="a@b.com")
         self.assertEqual(user.pk, "a@b.com")
