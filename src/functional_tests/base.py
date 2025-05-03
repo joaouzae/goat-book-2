24914c238ff75968b378fae7d6a378dbd3c5f5ca
Joao Carlos <joao_uzae@hotmail.com>
2025-05-02 22:22:48 -0300

    adiciona wait helper function

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -8,6 +8,20 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 20
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,6 +32,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @wait
     def wait_for(self, fn):
         start_time = time.time()
         while True:
@@ -44,6 +59,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element(By.ID, "id_text")
 
+    @wait
     def wait_to_be_logged_in(self, email):
         self.wait_for(
             lambda: self.browser.find_element(By.CSS_SELECTOR, "#id_logout"),
