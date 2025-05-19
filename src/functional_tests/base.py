19a29923e3fcd8a97f82855e95b03e47ed6adb77
Joao Carlos <joao_uzae@hotmail.com>
2025-05-18 22:07:45 -0300

    add helper to create list items in FTs

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -3,6 +3,7 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 from functional_tests.container_commands import reset_database
@@ -57,3 +58,10 @@ class FunctionalTest(StaticLiveServerTestCase):
     def wait_to_be_logged_out(self, email):
         navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
         self.assertNotIn(email, navbar.text)
+
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements(By.CSS_SELECTOR, "#id_list_table tr"))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f"{item_number}: {item_text}")
