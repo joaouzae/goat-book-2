7e23d85eebd1464df9bd61a4e3a826724cbd804a
Joao Carlos <joao_uzae@hotmail.com>
2025-06-21 23:22:18 -0300

    sharee can see shared list (not just owner)
    depende do próximo commit

--- src/lists/views.py
+++ src/lists/views.py
@@ -34,7 +34,11 @@ def view_list(request, list_id):
             form.save()
             return redirect(our_list)
     else:
-        if request.user.is_authenticated and our_list.owner == request.user:
+        if (
+            request.user.is_authenticated
+            and request.user == our_list.owner
+            or request.user in our_list.shared_with.all()
+        ):
             form = ExistingListItemForm(for_list=our_list)
         else:
             return render(request, "not_allowed.html")
