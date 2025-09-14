a66b6e7487e8c60c817bd44bc6cd9196045eb226
Joao Carlos <joao_uzae@hotmail.com>
2025-09-13 21:46:14 -0300

    include python anywhere site in allowed_hosts

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -17,14 +17,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-if "DJANGO_DEBUG_FALSE" in os.environ:
-    DEBUG = False
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
-else:
-    DEBUG = True
-    SECRET_KEY = "insecure-key-for-dev"
-    ALLOWED_HOSTS = []
+DEBUG = True
+SECRET_KEY = "insecure-key-for-dev"
+ALLOWED_HOSTS = ["joaouzae.pythonanywhere.com"]
 
 
 # Application definition
